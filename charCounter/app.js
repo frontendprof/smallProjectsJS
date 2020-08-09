3d5d8c8a8f0fd3c5e93ccccd5ea0c2e43521575a
AbdulMalik <abdumaliksharipov@gmail.com>
2020-08-09 19:26:57 +0500

    Complete

--- charCounter/app.js
+++ charCounter/app.js
@@ -16,16 +16,22 @@ const output=document.querySelector(".output");
 })
 
 
-const maxLength=10;
-const warnLength=5;
+const maxLength=20;
+const warnLength=7;
 
 
 function textCounter(e){
-    console.log(e);
     let count=txt.value.length;
-    console.log(count);
 
     if(count>maxLength){
-        console.log(txt.value.substring(0,maxLength))
+        txt.value=txt.value.substring(0,maxLength);
     }
+
+    if(count>warnLength){
+        output.classList.add("red");        
+    }
+    else{
+        output.classList.remove("red");
+    }
+    output.innerHTML=(maxLength-count)+" characters left";
 }
